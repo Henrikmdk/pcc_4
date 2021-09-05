3914079099d1e846bef27182b9ae42d2122af902
henrik <henrik.mortensen79@gmail.com>
2021-09-06 00:20:24 +0200

    kapitel 4 ver.2.1
    changelog:
    Line length fixed with the use of soft wrap
    Fixed docstring comments wrongly used - made #comments instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-'''4-1 looping through a list of pizzas'''
+#4-1 looping through a list of pizzas
 count = 1
 pizzas = ["Meat lover", "Bianca", "Milan", "Quattro stagione", "Havaii", "Pronto"]
 print("Our Pizzas")
@@ -8,9 +7,13 @@ for pizza in pizzas:
     count += 1
 print("Happy hour every wednesday 3-4am\n")
 
-'''4-2 looping through a list of animals'''
+#4-2 looping through a list of animals
 dwelling_fish =["Fjæsing", "Tunge", "Tobis"]
-descriptions = [" - Har giftpigge på rygfinne og gællelåg. Den er forøvrigt yderst velsmagende som spisefisk", " - En meget velsmagende fladfisk med stor nytteværdi pga et lille hoved. Den giver relativt store filetter", " - Brugt af lystfiskere som agnfisk og af fiskeindustrien til olie og dyrefodder", "Disse tre fisk findes nedgravet i sandet om dagen og kan være svære at se. Om natten svømmer de i den fri vandsøjle!\n"]
+descriptions = [" - Har giftpigge på rygfinne og gællelåg. Den er forøvrigt yderst velsmagende som spisefisk",
+                " - En meget velsmagende fladfisk med stor nytteværdi pga et lille hoved. Den giver relativt "
+                "store filetter", " - Brugt af lystfiskere som agnfisk og af fiskeindustrien til olie og dyrefodder",
+                "Disse tre fisk findes nedgravet i sandet om dagen og kan være svære at se. Om natten svømmer de i "
+                "den fri vandsøjle!\n"]
 for fish in dwelling_fish:
     if fish == "Fjæsing":
         print(fish + descriptions[0])
@@ -20,7 +23,7 @@ for fish in dwelling_fish:
         print(fish + descriptions[2])
 print(descriptions[3])
 
-'''4-3 counting to twenty using a range list and for-loop'''
+#4-3 counting to twenty using a range list and for-loop
 numbers = list(range(1, 21))
 for number in numbers:
     print(number)
@@ -32,25 +35,25 @@ for number in more_numbers:
     print(number)
 print("\n")'''
 
-'''4-5 one million worked on'''
+#4-5 one million worked on
 even_more_numbers = list(range(1, 1_000_001))
 print(min(even_more_numbers))
 print(max(even_more_numbers))
 print(sum(even_more_numbers)/1_000_000)
 print("\n")
 
-'''4-6 odd numbers'''
+#4-6 odd numbers
 odd_numbers = list(range(1,21,2))
 for odds in odd_numbers:
     print(odds)
 print("\n")
-'''4-7 threes, list of multiples from 3 to 30'''
+#4-7 threes, list of multiples from 3 to 30
 threes_numbers = list(range(3, 31, 3))
 for threes in threes_numbers:
     print(threes)
 print("\n")
 
-'''4-8 cubes'''
+#4-8 cubes
 cubes =[]
 for value in range(1, 11):
     cube = value**3
@@ -58,13 +61,13 @@ for value in range(1, 11):
     print(cube)
 print("\n")
 
-'''4-9 cubes with comprehension'''
+#4-9 cubes with comprehension
 cubes_comp = [value**3 for value in range(1, 11)]
 for cube in cubes_comp:
     print(cube)
 print("\n")
 
-'''4-10 slices'''
+#4-10 slices
 #using existing list from 4-1
 print(f"The first three items in the list 'pizzas' are: {pizzas[:3]}")
 middle = len(pizzas)//2
@@ -72,7 +75,7 @@ print(f"three items from the middle of 'pizzas' {pizzas[middle-1:middle+2]}")
 print(f"three items from the end of the list 'pizzas' {pizzas[-3:]}")
 print("\n")
 
-'''4-11 my pizzas, your pizzas'''
+#4-11 my pizzas, your pizzas
 #creating a copy of pizzas
 friend_pizzas = pizzas[:]
 #adding to the origin
@@ -89,18 +92,22 @@ for pizza in friend_pizzas:
     print(pizza)
 print("\n")
 
-'''4-12 more loops'''
+#4-12 more loops
 #I believe i added the for-loops correctly in the first place?!
 
-'''4-13 tuple buffet'''
-buffet = ("Oksetyndsteg med fuglegræs, syltede skalotter og timianstøv", "Rødvinssauce med madargaskarpeber", "Honningglasserede gulerødder med salvie", "ovnstegte kartofler vendt i mos og olivenolie", "Bagte tomater, semidried, thai basilikum og emmenthaler")
+#4-13 tuple buffet
+buffet = ("Oksetyndsteg med fuglegræs, syltede skalotter og timianstøv", "Rødvinssauce med madargaskarpeber",
+          "Honningglasserede gulerødder med salvie", "ovnstegte kartofler vendt i mos og olivenolie",
+          "Bagte tomater, semidried, thai basilikum og emmenthaler")
 print("Buffet:")
 for element in buffet:
     print(element)
 print("\n")
 
 #menu change
-buffet = ("Sprængt kalkun cuvette, syltede skalotter og grillet citron", "Skysauce med madargaskarpeber", "Honningglasserede gulerødder med salvie", "ovnstegte kartofler vendt i mos og olivenolie", "Bagte tomater, semidried, thai basilikum og emmenthaler")
+buffet = ("Sprængt kalkun cuvette, syltede skalotter og grillet citron", "Skysauce med madargaskarpeber",
+          "Honningglasserede gulerødder med salvie", "ovnstegte kartofler vendt i mos og olivenolie",
+          "Bagte tomater, semidried, thai basilikum og emmenthaler")
 print("Buffet:")
 for element in buffet:
     print(element)
